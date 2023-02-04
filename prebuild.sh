a0b000d3dff1c528d105a5eedd7360e730d998bf
bailwillharr <baileyharrison04@outlook.com>
2023-02-04 13:39:20 +0000

    Edit prebuild script

--- prebuild.sh
+++ prebuild.sh
@@ -4,6 +4,7 @@ mkdir -p Debug
 cd Debug
 cmake -G Ninja -D CMAKE_BUILD_TYPE=Debug -D CMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 cd ..
+ln -sf Debug/compile_commands.json .
 mkdir -p Release
 cd Release
 cmake -G Ninja -D CMAKE_BUILD_TYPE=Release -D CMAKE_EXPORT_COMPILE_COMMANDS=ON ..
